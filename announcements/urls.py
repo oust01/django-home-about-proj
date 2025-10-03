9027bc465f2240dd65eb60668e6fe459737176a1
Austin Jeru D. Amascual <your-email@example.com>
2025-10-03 22:03:57 +0800

    Finished announcements site with navbar and read more toggle

--- announcements/urls.py
+++ announcements/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('announcements/', views.announcements_list, name='announcement_list'),
-    path('announcements/<int:id>/', views.announcements_detail, name='announcement_detail'),
+    path('announcements/<int:id>/', views.announcement_detail, name='announcement_detail'),
 ]
