ee7948613101bb6f6c6c2c44ea6fcbd513e3538e
Austin Jeru D. Amascual <your-email@example.com>
2025-10-03 21:51:45 +0800

    Finished announcements site with navbar and read more toggle

--- announcements/urls.py
+++ announcements/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # homepage
-    path('announcements/', views.announcement_list, name='announcement_list'),  # list page
-    path('announcements/<int:id>/', views.announcement_detail, name='announcement_detail'),  # detail page
+    path('', views.home, name='home'),
+    path('announcements/', views.announcement_list, name='announcement_list'),
+    path('announcements/<int:id>/', views.announcement_detail, name='announcement_detail'),
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import render
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", lambda request: render(request, "home.html"), name="home"),  # Home page
-    path("announcements/", include("announcements.urls")),  # Announcements list & detail
+    path('admin/', admin.site.urls),
+    path('', include('announcements.urls')),
 ]
